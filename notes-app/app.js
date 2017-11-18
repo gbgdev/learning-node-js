c6164d15695e6e1540e7636c8d396010a4160956
GG <gbgdev@gmail.com>
2017-11-18 18:27:32 -0200

    Making notes-app use notes module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,18 +1,24 @@
 const notes = require('./notes.js');
 const yargs = require('yargs');
 
-var command = process.argv[2];
+args = yargs.argv;
+
+var command = args._[0];
 
 console.log(`Command: ${command}.`);
 
 if (command === 'add') {
   console.log('Adding new note.');
+  notes.addNote(args.title, args.body);
 } else if (command === 'list') {
   console.log('Listing notes.');
+  notes.getAll();
 } else if (command === 'read') {
   console.log('Reading note.');
+  notes.getNote(args.title);
 } else if (command === 'remove') {
   console.log('Removing note.');
+  notes.removeNote(args.title);
 } else {
   console.log('Unkwnow command.');
 }
