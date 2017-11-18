addd222252beef93a47a341607f2a93a0c356b49
GG <gbgdev@gmail.com>
2017-11-18 19:07:29 -0200

    Implementing rough addNote function

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,27 @@
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log('Adding note', title, body);
+  var node = {
+    title,
+    body
+  }
+
+  var notes = [];
+
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    var notes = JSON.parse(notesString);
+  } catch (e) {
+
+  }
+
+  var duplicatedNotes = notes.filter((note) => note.title === title);
+
+  if (duplicatedNotes.length === 0) {
+    notes.push(node);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+      console.log('Note saved successfully');
+  }
 };
 
 var getAll = () => {
