d829d9e56fb1c74c3b8d091cc5e698cc83cbf37a
GG <gbgdev@gmail.com>
2017-11-18 18:50:54 -0200

    Minor

--- 7-json/app.js
+++ 7-json/app.js
@@ -1,5 +1,5 @@
 /*
- * This example cover an alternative strategy to save data using JSON, in order to avoid the use of database (for a while).
+ * This example cover an alternative strategy that was used in the course to save data using JSON, in order to avoid the use of database.
  */
 
 const fs = require('fs');
